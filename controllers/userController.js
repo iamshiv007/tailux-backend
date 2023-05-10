ee605fa1cefd1602bd3813a77c97bb7cd686bac1
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-10 17:46:33 +0530

    /me/update route completed or profileUpadate

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,7 @@ const userController = {
                 httpOnly: true
             }
 
-            res.cookie('token', token, options).json({ success: true, user })
+            res.cookie('token', token, options).status(201).json({ success: true, user })
 
         } catch (error) {
             res.status(500).json(error)
@@ -91,6 +91,16 @@ const userController = {
     getUserDetails: async (req, res) => {
         try {
             res.status(200).json({ success: true, user: req.user })
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+
+    updateProfile: async (req, res) => {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.user.id, req.body)
+
+            res.status(200).json({ success: true, messege:'Profile Updated' })
         } catch (error) {
             res.status(500).json(error)
         }
--- routes/user.js
+++ routes/user.js
@@ -8,5 +8,7 @@ router.route("/register").post((req, res) => userController.register(req, res))
 router.route("/login").post((req, res) => userController.login(req, res))
 router.route("/me").get(isAuthenticatedUser, (req, res) => userController.getUserDetails(req, res))
 router.route("/logout").get((req, res) => userController.logout(req, res))
+router.route("/me/update").patch(isAuthenticatedUser, (req, res) => userController.updateProfile(req, res))
 
 module.exports = router;
+

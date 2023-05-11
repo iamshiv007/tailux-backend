f6c067846592ae0843f3931148e9927cf3e5633c
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-11 15:28:14 +0530

    getUserAddresses completed

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,5 +1,6 @@
 const Address = require("../models/address")
 
+// 1. Create New Address
 exports.newAddress = (req, res) => {
     const userId = req.user._id
     const { ...data } = req.body
@@ -8,3 +9,11 @@ exports.newAddress = (req, res) => {
         .then((address) => res.status(201).json({ success: true, address }))
         .catch((err) => res.status(500).json(err))
 }
+
+// 2. Get User addresses
+exports.getUserAddresses = (req, res) => {
+    Address.find({user:req.user._id})
+    .then((addresses) => res.status(200).json({success:true, addresses}))
+    .catch((err) => res.status(500).json(err))
+}
+
--- routes/address.js
+++ routes/address.js
@@ -1,8 +1,9 @@
-const { newAddress } = require("../controllers/addressController")
+const { newAddress, getUserAddresses } = require("../controllers/addressController")
 const { isAuthenticatedUser } = require("../middleware/auth")
 
 const router = require("express").Router()
 
 router.route('/address/new').post(isAuthenticatedUser, newAddress)
+router.route('/addresses').get(isAuthenticatedUser, getUserAddresses)
 
 module.exports = router

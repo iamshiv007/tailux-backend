beb176c1275ab8cca9209ccfa5901ad620572b21
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-11 10:19:38 +0530

    updateProduct and deleteProduct Completed

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,21 +3,58 @@ const Product = require('../models/product')
 // 1. New Product -- Admin
 exports.createProduct = (req, res) => {
     Product.create(req.body)
-    .then((product) => res.status(201).json({success:true, product}))
-    .catch((err) => res.status(500).json(err))
+        .then((product) => res.status(201).json({ success: true, product }))
+        .catch((err) => res.status(500).json(err))
 }
 
 // 2. Get All Products
 exports.getAllProducts = (req, res) => {
     Product.find()
-    .then((products) => res.status(200).json({success:true, products}))
-    .catch((err) => res.status(500).json(err))
+        .then((products) => res.status(200).json({ success: true, products }))
+        .catch((err) => res.status(500).json(err))
 }
 
 // 3. One Product Details
-exports.getOneProductDetails = (req, res) => {
-    Product.findById(req.params.id)
-    .then((product) => res.status(200).json({success:true, product}))
-    .catch((err) => res.status(500).json(err))
+exports.getOneProductDetails = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product)
+            return res.status(404).json({ success: false, error: "Product Not Found" })
+
+        res.status(200).json({ success: true, product })
+
+    } catch (error) {
+        res.status(500).json(error)
+
+    }
 }
 
+// 4. Update Product -- Admin
+exports.updateProduct = async (req, res) => {
+
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body)
+        if (!product)
+            return res.status(404).json({ success: false, error: "Product Not Found" })
+
+        res.status(200).json({ success: true, message: "Product Updated" })
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// 5. Delete Product
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id)
+        if (!product)
+            return res.status(404).json({ success: false, error: "Product Not Found" })
+
+        res.status(200).json({ success: true, message: "Product Deleted" })
+
+    } catch (error) {
+        res.status(500).json(error)
+
+    }
+}
--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,4 @@
-const { createProduct, getAllProducts, getOneProductDetails } = require("../controllers/productController")
+const { createProduct, getAllProducts, getOneProductDetails, updateProduct, deleteProduct } = require("../controllers/productController")
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth")
 
 const router = require("express").Router()
@@ -6,5 +6,7 @@ const router = require("express").Router()
 router.route("/admin/product/new").post(isAuthenticatedUser, authorizeRole, createProduct)
 router.route("/products").get(getAllProducts)
 router.route("/product/:id").get(getOneProductDetails)
+router.route("/admin/product/:id").patch(isAuthenticatedUser, authorizeRole, updateProduct)
+router.route("/admin/product/:id").delete(isAuthenticatedUser, authorizeRole, deleteProduct)
 
 module.exports = router

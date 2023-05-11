8abf193e1f0e20a1c44f6dd1fc6530df8107de19
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-11 15:55:00 +0530

    deleteAddress and updateAddress completed

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -17,3 +17,27 @@ exports.getUserAddresses = (req, res) => {
     .catch((err) => res.status(500).json(err))
 }
 
+// 3. Delete A Address
+exports.deleteAddress = async(req, res) => {
+    try {
+        const address = await Address.findByIdAndDelete(req.params.id)
+        if(!address)
+        return res.status(404).json({success:false, error:"Address Not Found"})
+
+        res.status(200).json({success:true, message:"Address Deleted"})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+exports.upadateAddress = async(req, res) => {
+    try {
+        const address = await Address.findByIdAndUpdate(req.params.id, req.body) 
+        if(!address)
+        return res.status(404).json({success:false, error:"Address Not Found"})
+
+        res.status(200).json({success:true, message:"Address Updated"})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
--- routes/address.js
+++ routes/address.js
@@ -1,9 +1,11 @@
-const { newAddress, getUserAddresses } = require("../controllers/addressController")
+const { newAddress, getUserAddresses, deleteAddress, upadateAddress } = require("../controllers/addressController")
 const { isAuthenticatedUser } = require("../middleware/auth")
 
 const router = require("express").Router()
 
 router.route('/address/new').post(isAuthenticatedUser, newAddress)
 router.route('/addresses').get(isAuthenticatedUser, getUserAddresses)
+router.route('/address/:id').delete(isAuthenticatedUser, deleteAddress)
+router.route('/address/:id').patch(isAuthenticatedUser, upadateAddress)
 
 module.exports = router

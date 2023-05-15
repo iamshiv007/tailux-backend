c936d4f85a180ed7a963780cf734b55d41af142e
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-15 14:06:59 +0530

    getUserCartItems function completed

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,9 +1,24 @@
 const Cart = require('../models/cart')
 
-// Add to cart
+// 1. Add to cart
 exports.addToCart = (req, res) => {
     Cart.create(req.body)
-    .then((cart) => res.send({ success:true, cart}))
-    .catch((err) => res.send({ succes:false, err}))
+        .then((cart) => res.stattus(201).json({ success: true, cart }))
+        .catch((err) => res.status(500).json({ succes: false, err }))
 }
+
+// 2. Get User Cart List
+exports.getUserCartItems = (req, res) => {
+    Cart.find({ user: req.user._id })
+    .populate("user")
+    .populate("product")
+    .then((cartItems) => {
+      res.status(200).json({ success: true, cartItems });
+    })
+    .catch((err) => {
+      res.status(500).json({ success: false, err });
+    });
+}
+
+
 
